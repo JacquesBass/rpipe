073b8d7812e341b03dd70454cd0a8288b57d5c0c
JacquesBass <jacques@dybot.com>
2017-09-20 13:45:50 +0200

    Follows the new "standard" for output scripts.

--- scripts/007_OUTPUT_example_predict.R
+++ scripts/007_OUTPUT_example_predict.R
@@ -9,7 +9,12 @@
 
 #>> There is no imposed structure for OUTPUT scripts. One script can create more than one file.
 
+write_example_predict <- function(fn)
+{
+    cat('Writing predictions to output/my_preditions.csv ...')          # Nice to recall the output file name to the user.
+    write.csv(data.frame(x = 1:3), file = fn)
+    cat('Ok.\n')
+}
 
-cat('Writing predictions to output/my_preditions.csv ...')          # Nice to recall the output file name to the user.
-write.csv(data.frame(x = 1:3), file = 'output/my_preditions.csv')
-cat('Ok.\n')
+
+write_example_predict('output/example_001.csv')
