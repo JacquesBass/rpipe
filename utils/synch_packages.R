77e05eac10548d9e5853e5df58230b91ecb13c5d
Mike <miguel@dybot.com>
2017-12-20 14:43:53 +0100

    waterfalls and xgboostExplainer added.

--- utils/synch_packages.R
+++ utils/synch_packages.R
@@ -48,8 +48,8 @@ packs <- c('abind', 'acepack', 'ade4', 'arules', 'assertthat', 'audio', 'AzureML
            'strucchange', 'survival', 'tabplot', 'tau', 'tcltk', 'tclust', 'TeachingDemos', 'tensorflow', 'testthat', 'text2vec', 'textcat', 'tfruns',
            'TH.data', 'tibble', 'tidyr', 'tidyselect', 'timeDate', 'timeline', 'timeSeries', 'titanic', 'tm', 'tools', 'transcribeR', 'tree',
            'treeClust', 'triangle', 'trimcluster', 'tseries', 'tsne', 'TSP', 'TTR', 'tuneR', 'twitteR', 'upclass', 'USAboundaries',
-           'USAboundariesData', 'utils', 'uuid', 'vcd', 'viridis', 'viridisLite', 'visNetwork', 'whisker', 'withr', 'wmtsa', 'wordcloud', 'xgboost',
-           'XML', 'xml2', 'xtable', 'xts', 'yaml', 'zipfR', 'zoo')
+           'USAboundariesData', 'utils', 'uuid', 'vcd', 'viridis', 'viridisLite', 'visNetwork', 'waterfalls', 'whisker', 'withr', 'wmtsa',
+           'wordcloud', 'xgboost', 'xgboostExplainer', 'XML', 'xml2', 'xtable', 'xts', 'yaml', 'zipfR', 'zoo')
 
 
 if (length(packs) != length(unique(packs))) stop('Duplicated.')
